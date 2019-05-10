71a17749f06560347ea760208bbeae38de86a96b
michaelConnectized <michael@connectized.com>
2019-05-10 19:22:07 +0800

    update version 1.8

--- app/src/main/java/com/example/user/markingactivity/confirmAdapter.java
+++ app/src/main/java/com/example/user/markingactivity/confirmAdapter.java
@@ -1,8 +1,10 @@
 package com.example.user.markingactivity;
 
 import android.content.Context;
+import android.opengl.Visibility;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -18,6 +20,10 @@ public class confirmAdapter extends bleAdapter {
         View confirmView = super.getView(position, view, viewGroup);
         confirmView.findViewById(R.id.chk_check).setVisibility(View.GONE);
         ((TextView)confirmView.findViewById(R.id.tv_uuid)).setText(currentDevice.getmUUID());
+
+        Button btn_edit_txPwr = confirmView.findViewById(R.id.btn_edit_txPwr);
+        btn_edit_txPwr.setVisibility(View.GONE);
+
         return confirmView;
     }
 }
